3a5ec1ab575f92a55620d257fd0213dac1531e98
Maxim <makson202007@gmail.com>
2021-06-06 02:33:33 +0300

    Improved program to process brackets

--- Laboratorna3OP.cpp
+++ Laboratorna3OP.cpp
@@ -11,18 +11,20 @@ private:
 public:
     stack()
     {
-        p_t_c = 0;
-        p_t_i = 0;
+        p_t_c = -1;
+        p_t_i = -1;
     }
     char pop_c()
     {
         p_t_c--;
-        return STACK_CHAR[p_t_c];
+        return STACK_CHAR[p_t_c+1];
+        
     }
     void push_c(char v)
     {   
+        p_t_c++;
         STACK_CHAR[p_t_c]=v;
-        p_t_c++;   
+        
     }
     int check_pt_c()
     {
@@ -31,14 +33,14 @@ public:
     int pop_i()
     {
         p_t_i--;
-        return STACK_INT[p_t_i];
+        return STACK_INT[p_t_i+1];
     }
     void push_i(int v)
     {
-        STACK_INT[p_t_i] = v;
         p_t_i++;
+        STACK_INT[p_t_i] = v;
     }
-    int check_pt_i()
+    int check_top_i()
     {
         return p_t_i;
     }
@@ -46,31 +48,35 @@ public:
 
 std::string console_interp(int, char*[]);
 int num_of_elements(std::string);
-std::string* divide_into_elements(std::string, int*);
-std::string* sort_station(std::string*, int);
+std::string* divide_into_elements(std::string, int);
+std::string* sort_station(std::string*, int*);
 char priority(char, char);
 int get_through(std::string*, int);
 int pow(int, int);
 
 int main(int _argc, char* _argv[])
 {
-    //std::string infix = console_interp(_argc, _argv);
-    std::string infix;
-    getline(std::cin, infix);
+    std::string infix = console_interp(_argc, _argv);
+    //std::string infix; - debug testing
+    //getline(std::cin, infix);
     
     int n = num_of_elements(infix);
-    std::string* infix_alg = divide_into_elements(infix, &n);
+    std::string* infix_alg = divide_into_elements(infix, n);
     for (int i = 0; i < n; i++)
     {
         std::cout << infix_alg[i] << std::endl;
     }
 
-    std::string* out = sort_station(infix_alg, n);
-    
+    std::string* out_temp = sort_station(infix_alg, &n);
+    std::string* out = new std::string[n];
+    out = out_temp;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << out[i] << std::endl;
+    }
+
     int sum = get_through(out, n);
     std::cout << sum << std::endl;
-
-
 }
 
 std::string console_interp(int argc, char* argv[]) //interpreting console input
@@ -94,7 +100,7 @@ int num_of_elements(std::string S) //getting number of elements
     }
     for (i = 1; i < S.length(); i ++)
     {
-        if ((isdigit(S[i]) == 0) and ((isdigit(S[i+1]))==0))
+        if (((isdigit(S[i]) == 0) and ((isdigit(S[i + 1])) == 0)) or (S.length() - 1 == i))
         {
             count_el++;
         }
@@ -107,9 +113,8 @@ int num_of_elements(std::string S) //getting number of elements
     return count_el;
 }
 
-std::string* divide_into_elements(std::string D, int* n_ext) //dividing into operators and operands
+std::string* divide_into_elements(std::string D, int n) //dividing into operators and operands
 {
-    int n = *n_ext;
     std::string* L = new std::string[n];
     int i = 0;
     int k = 0;
@@ -118,30 +123,48 @@ std::string* divide_into_elements(std::string D, int* n_ext) //dividing into ope
     {
         j = 1;
     }
+    else if (D[0] == '(')
+    {
+        L[i] = D.substr(0, 1);
+        j = 1;
+        k = 1;
+        i++;
+    }
     while (j < D.length())
     {
-        if ((isdigit(D[j]) == 0) and (D[j] == '(') and (D[j+1] == '-'))
+        if ((isdigit(D[j]) == 0) and (k - j == 0))
         {
+            L[i] = D.substr(j, 1);
+            k++;
+            j++;
+            i++;
+        }
+        else if ((isdigit(D[j]) == 0) and (D[j] == '(') and (D[j+1] == '-'))
+        {
+            k = j;
             j += 2;
-            k = j-1;
+            L[i] = D.substr(k, 1);
+            i++;
+            k++;
             while (isdigit(D[j]) != 0)
             {
                 j++;
             }
             L[i] = D.substr(k, j - k);
+            i++;
             k = j;
-            *n_ext -= 1;
             if (D[j] == ')')
             {
-                k++;
+                L[i] = D.substr(k, 1);
                 j++;
-                *n_ext-=1;
+                k++;
+                i++;
             }
-            i++;
             if (j >= D.length())
             {
-                break;
+                return L;
             }
+            
             L[i] = D.substr(k, 1);
             k++;
             i++;
@@ -161,8 +184,9 @@ std::string* divide_into_elements(std::string D, int* n_ext) //dividing into ope
     return L;
 }
 
-std::string* sort_station(std::string* L, int n) //Shunting-Yard sort
+std::string* sort_station(std::string* L, int* n_ext) //Shunting-Yard algorithm
 {
+    int n = *n_ext;
     stack ST;
     char l;
     std::string* out = new std::string[n];
@@ -174,35 +198,52 @@ std::string* sort_station(std::string* L, int n) //Shunting-Yard sort
         {
             out[i] = L[j];
             i++;
-            j++;
+        }
+        else if ((L[j][0]) == '(')
+        {
+            ST.push_c(L[j][0]);
+        }
+        else if ((L[j][0]) == ')')
+        {
+            while (out[i-1] != "(")
+            {
+                out[i]=ST.pop_c();
+                i++;
+            }
+            out[i - 1] == "";
+            i--;
+            *n_ext -= 2;
         }
         else
         {
-            if (ST.check_pt_c() == 0)
+            if ((ST.check_pt_c() == -1))
             {
                 ST.push_c(L[j][0]);
-                j++;
             }
             else
             {
                 l = ST.pop_c();
-                if (priority(l, L[j][0]) == l)
+                if (l == '(')
+                {
+                    ST.push_c(l);
+                    ST.push_c(L[j][0]);
+                }
+                else if (priority(l, L[j][0]) == l)
                 {
                     out[i] = l;
                     ST.push_c(L[j][0]);
                     i++;
-                    j++;
                 }
                 else
                 {
                     ST.push_c(l);
                     ST.push_c(L[j][0]);
-                    j++;
                 }
             }
         }
+        j++;
     }
-    while (ST.check_pt_c() > 0)
+    while (ST.check_pt_c() != -1)
     {
         out[i] = ST.pop_c();
         i++;
